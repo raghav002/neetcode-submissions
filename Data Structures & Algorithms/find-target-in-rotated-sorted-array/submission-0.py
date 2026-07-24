4e9f5983b10611ea518f8ce6dfcc233746d25d99
Raghav Jindal <54995960+raghav002@users.noreply.github.com>
2026-07-24 14:42:49 +0800

    Add: find-target-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # input: array len n sorted in ascending order
+        #        now rotated between 1 and n times
+        # output: return index of target within nums if present else -1
+        # constraints: assume nums elements are unique
+        #              O(logn) time
+
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
