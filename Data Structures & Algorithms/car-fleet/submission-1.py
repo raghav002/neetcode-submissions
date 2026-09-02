a326b3ce6c366352cd1444386b8b2df1aed75852
Raghav Jindal <54995960+raghav002@users.noreply.github.com>
2026-09-02 10:17:10 -0500

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # input: two int arrays - position and speed. the ith value of both indicates
+        #                         the position and speed of the ith car respectively
+        # output: the number of sets of cars that are driving at the same position 
+        #         and same speed that will arrive at the destination 
+        # constraints: a car cannot pass a car ahead of it. It can only match its
+        #              speed and destination at that point 
+
+        stats = [(p, s) for p, s in zip(position, speed)]
+        stats.sort(reverse=True)
+        stack = []
+        for i in range(len(position)):
+            stack.append((target - stats[i][0]) / stats[i][1])
+            if len(stack)>=2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+        
